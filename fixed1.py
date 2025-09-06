6bae3588abb6a1a6b118cd6a28127c34c82f5576
mge-bro <imbatop49@gmail.com>
2025-09-06 14:08:06 +0300

    Add files via upload

---
+++ fixed1.py
@@ -0,0 +1,34 @@
+import time
+
+b = 0
+while b == 0:
+    print('Введите число')
+    x = int(input())
+
+    print('Укажите знак (+, -, *, /)')
+    y = input()
+
+    print('Введите второе число')
+    z = int(input())
+
+    if   y == '+': m = x + z
+    elif y == '-': m = x - z
+    elif y == '*': m = x * z
+    elif y == '/':
+        if z == 0:
+            print('Ошибка: деление на ноль')
+            exit()
+        m = x / z
+    else:
+        print('Неверный оператор')
+        exit()
+
+
+    print('Результат:', m)
+    time.sleep(1)
+    print('Хотите еще? (да/нет)')
+    o = str(input())
+    if o == 'нет':
+        b = 1
+    else:
+        b = 0
